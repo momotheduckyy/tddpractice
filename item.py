3e11b97612149bcdcdbf1366cf9ed596e7d3418b
momotheduckyy <154822774+momotheduckyy@users.noreply.github.com>
2025-10-24 12:43:08 -0500

    Implement getter and setter methods in Item class
    Added getter and setter methods for name, price, and count attributes.

---
+++ item.py
@@ -0,0 +1,25 @@
+class Item:
+  
+    def __init__(self, name, price, count):
+        self.name = name
+        self.price = price
+        self.count = count
+      
+def get_name(self):
+        return self.name
+    
+    def set_name(self, name):
+        self.name = name
+    
+    def get_price(self):
+        return self.price
+    
+    def set_price(self, price):
+        self.price = price
+    
+    def get_count(self):
+        return self.count
+    
+    def set_count(self, count):
+        self.count = count
+  
